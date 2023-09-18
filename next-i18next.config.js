97e06a3d3801996327feb1189b659564888ee1f4
usasuke <kumasuke.usasu@gmail.om>
2023-09-18 16:17:28 +0900

    ja-JPの言語変更追加

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -2,8 +2,8 @@ const path = require('path');
 
 module.exports = {
   i18n: {
-    defaultLocale: 'en-US',
-    locales: ['en-US', 'de-DE'],
+    defaultLocale: 'ja-JP',
+    locales: ['ja-JP','en-US' ],
     localeDetection: false,
     localePath: path.resolve('./public/locales'),
   },
