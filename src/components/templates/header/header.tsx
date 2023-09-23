43919fe34d975a2beb11bf89a66c9799439a317f
usasuke <kumasuke.usasu@gmail.om>
2023-09-23 16:49:10 +0900

    GAの設定追加

--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -4,12 +4,14 @@ import Link from 'next/link';
 import BlogLogo from '@icons/blog-logo.svg';
 import { LanguageSelector } from '@src/components/features/language-selector';
 import { Container } from '@src/components/shared/container';
+import GoogleAnalytics from '@src/components/shared/GoogleAnalytics';
 
 export const Header = () => {
   const { t } = useTranslation();
 
   return (
     <header className="py-5">
+      <GoogleAnalytics />
       <nav>
         <Container className="flex items-center justify-between">
           <Link href="/" title={t('common.homepage')}>
