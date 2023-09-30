98732e3d26bc547cbe5e51b12122823f0e0d25cc
usasuke <kumasuke.usasu@gmail.om>
2023-10-01 00:42:28 +0900

    code snipet 追加

--- src/components/features/contentful/CtfRichText.tsx
+++ src/components/features/contentful/CtfRichText.tsx
@@ -1,10 +1,11 @@
 import { documentToReactComponents, Options } from '@contentful/rich-text-react-renderer';
 import { BLOCKS, Document, MARKS } from '@contentful/rich-text-types';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { atomOneLight } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 import { ArticleImage } from '@src/components/features/article';
 import { ComponentRichImage } from '@src/lib/__generated/sdk';
+import { Mark, Text } from '@contentful/rich-text-types/dist/types/types';
 
 export type EmbeddedEntryType = ComponentRichImage | null;
 
@@ -34,27 +35,22 @@ export const contentfulBaseRichTextOptions = ({ links }: ContentfulRichTextInter
       const entry = links?.entries?.block?.find(
         (item: EmbeddedEntryType) => item?.sys?.id === node.data.target.sys.id,
       );
-
       if (!entry) return null;
-
       return <EmbeddedEntry {...entry} />;
     },
     [BLOCKS.PARAGRAPH]: (node, children) => {
-      if (
-        node.content.length === 1
-        //&&
-        //node.content[0].marks.find((x) => x.type === "code")
-      ) {
+      if (node.content.length === 1 && node.content[0]['marks'].find(x => x.type === 'code')) {
         return <div>{children}</div>;
       }
       return <p>{children}</p>;
     },
   },
+
   renderMark: {
     [MARKS.CODE]: text => {
       return (
-        <SyntaxHighlighter language="javascript" style={atomOneLight} showLineNumbers>
-          {text}
+        <SyntaxHighlighter style={vscDarkPlus} language="javascript" PreTag="pre">
+          {String(text).split('\n').slice(0).join('\n')}
         </SyntaxHighlighter>
       );
     },
